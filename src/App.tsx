02f26af29c3c7f600096dd06a847f342b0706fad
ViAnd <vitalii.andrianov@icloud.com>
2024-07-05 11:02:15 +0300

    added base style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
-import Button from './components/Button/Button'
+import { MouseEvent, useState } from 'react';
+import Button from './components/Button/Button';
 
 function App() {
+	const [counter, setCounter] = useState<number>(0);
+
+	const addCounter = (e: MouseEvent) => {
+		console.log(e);
+	};
 
 	return (
 		<>
-			<Button onClick={() => console.log()}>Кнопка</Button>
+			<Button onClick={addCounter}>Кнопка</Button>
 		</>
-	)
+	);
 }
 
-export default App
+export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,26 +1,11 @@
-import { FC } from "react";
-import "./Button.module.css";
-import { ButtonProps } from "./Button.props";
-import cn from "classnames";
-
-export const ButtonAlt: FC<ButtonProps> = ({
-  className,
-  children,
-  ...props
-}) => {
-  return (
-    <button className={cn("button accent", className)} {...props}>
-      {children}
-    </button>
-  );
-};
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
+import cn from 'classnames';
 
 function Button({ children, className, ...props }: ButtonProps) {
-  return (
-    <button className={cn("button accent", className)} {...props}>
-      {children}
-    </button>
-  );
+	return (
+		<button className={cn(styles['button'], styles['accent'], className)} {...props}>{children}</button>
+	);
 }
 
 export default Button;
