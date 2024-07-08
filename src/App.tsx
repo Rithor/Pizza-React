4b6238ee70b01fa3c664cfb3fd13b962e21567fb
ViAnd <vitalii.andrianov@icloud.com>
2024-07-08 10:57:11 +0300

    added base layout, routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
+import Input from './components/Input/Input';
+import { Link, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
 	const [counter, setCounter] = useState<number>(0);
@@ -11,6 +16,9 @@ function App() {
 	return (
 		<>
 			<Button onClick={addCounter}>Кнопка</Button>
+			<Button appearence='big' onClick={addCounter}>Кнопка</Button>
+			<Input placeholder='Email' />
+
 		</>
 	);
 }
