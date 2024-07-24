44d5cccfe85cd2bca56889801cb2d0e7c0d93be5
ViAnd <vitalii.andrianov@icloud.com>
2024-07-24 14:46:50 +0300

    added Cart, user state

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -2,13 +2,20 @@ import { NavLink, Outlet, useNavigate } from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button';
 import cn from 'classnames';
-import { useDispatch } from 'react-redux';
-import { AppDispath } from '../../store/store';
-import { userActions } from '../../store/user.slice';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispath, RootState } from '../../store/store';
+import { getProfile, userActions } from '../../store/user.slice';
+import { useEffect } from 'react';
 
 export function Layout() {
 	const navigate = useNavigate();
 	const dispatch = useDispatch<AppDispath>();
+	const profile = useSelector((s: RootState) => s.user.profile);
+	const items = useSelector((s: RootState) => s.cart.items);
+
+	useEffect(() => {
+		dispatch(getProfile());
+	}, [dispatch]);
 
 	const logout = () => {
 		dispatch(userActions.logout());
@@ -19,8 +26,8 @@ export function Layout() {
 		<div className={styles['sidebar']}>
 			<div className={styles['user']}>
 				<img className={styles['avatar']} src="/avatar.png" alt="Аватар пользователя" />
-				<div className={styles['name']}>Антон Ларичев</div>
-				<div className={styles['email']}>alari@ya.ru</div>
+				<div className={styles['name']}>{profile?.name}</div>
+				<div className={styles['email']}>{profile?.email}</div>
 			</div>
 			<div className={styles['menu']}>
 				<NavLink to='/' className={({ isActive }) => cn(styles['link'], {
@@ -31,7 +38,8 @@ export function Layout() {
 				<NavLink to='/cart' className={({ isActive }) => cn(styles['link'], {
 					[styles.active]: isActive
 				})}>
-					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</NavLink>
+					<img src="/cart-icon.svg" alt="Иконка корзины" />Корзина <span className={styles['cart-count']}>{items.reduce((acc, item) => acc += item.count, 0)}</span></NavLink>
+
 			</div>
 			<Button className={styles['exit']} onClick={logout}>
 				<img src="/exit-icon.svg" alt="Иконка выхода" />
